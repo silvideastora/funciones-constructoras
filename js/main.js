1779b47a78f81ef288660358ab0566a3345a3d49
Silvia Rodriguez <silvideastora18@gmail.com>
2021-03-31 18:30:48 -0600

    ADD 15days

--- js/main.js
+++ js/main.js
@@ -1,191 +1,112 @@
-/*function Koder(name, lastName, generation, birthDate, bootcamp, scores=[90, 80, 95, 100]) {
-    this.name = name;
-    this.lastName = lastName;
-    this.generation = generation;
-    this.birthDate = birthDate;
-    this.country = country;
-    this.bootcamp = bootcamp;
-    this.school = 'Kodemia';//
-    this.scores = scores;
-    this.greet = function() {
-        console.log(`Hola Mi nombre es ${name} y soy de la generación ${this.generation}º de ${this.bootcamp}`)
-    }//
-    this.asking = function(question, mentor) {
-        console.log(`${mentor} tengo duda sobre: ${question}`)
-    this.getAge = function (){
-        let today = new Date();
-        let birthDate = new Date(this.birthDate);
-        let age = today.getFullYear() - birthDate.getFullYear()
-    
-    }
-    }//
-//}
-
-//const koderDiana = new Koder('Diana', 'Ibañez', 11, '1992/06/03', 'Peru', 'JavaScript', 'KoderHause')
-//const koderTaquito = new Koder('Vero', 'Paredes', 11, '1992/06/03', 'Mexico', 'JavaScript')
-
-
-
-console.log(koderDiana)
-console.log(koderTaquito)*/
-
-const productCollection = [
+const product = [
     {
-        name: "Leche",
-        description: "Carton",
-        price: 16,
-        clasification: "Lacteo",
-        earning: 20,
-        pricePublic: 0,
-        price6LastMonth: [
-            {
-                month: "january",
-                priceMonth: 15,
-            },
-            {
-                month: "February",
-                priceMonth: 15,
-            },
-            {
-                month: "March",
-                priceMonth: 14,
-            },
-            {
-                month: "April",
-                priceMonth: 15,
-            },
-            {
-                month: "May",
-                priceMonth: 16,
-            }, 
-            {
-                month: "June",
-                priceMonth: 16,
-            }
-        ],
-        
-        soldThisWeek: 15,
-        caducityDate: 01/06/2021,
+        name: 'Leche', 
+        description: 'Un carton de leche',
+        price: 17.90,
+        clasification: 'Lacteo',
+        earningPorcentage: '.30',
+        priceSales: 0,
+        sixMonthsPrices: [24.0, 24.20, 24.50, 25.0, 25.20, 25.50],
+        quantitySold: 8,
+        caducity: '2021-04-15'
     },
     {
-        name: "Yogurth",
-        description: "Envase",
-        price: 10,
-        clasification: "Lacteo",
-        earning: 20,
-        pricePublic: 0,
-        price6LastMonth: [
-            {
-                month: "january",
-                priceMonth: 9,
-            },
-            {
-                month: "February",
-                priceMonth: 12,
-            },
-            {
-                month: "March",
-                priceMonth: 11,
-            },
-            {
-                month: "April",
-                priceMonth: 10,
-            },
-            {
-                month: "May",
-                priceMonth: 10,
-            }, 
-            {
-                month: "June",
-                priceMonth: 12,
-            }
-        ],
-        
-        soldThisWeek: 7,
-        caducityDate: 01/05/2021,
+        name: 'Yogurth', 
+        description: 'Un bote de 1 lt',
+        price: 30.0,
+        clasification: 'Lacteo',
+        earningPorcentage: '.25',
+        priceSales:0,
+        sixMonthsPrices: [38.0, 38.20, 38.50, 39.0, 39.50, 39.80],
+        quantitySold: 12,
+        caducity: '2021-04-21'
     },
     {
-        name: "Frijoles",
-        description: "Lata",
-        price: 13,
-        clasification: "perecedero",
-        earning: 20,
-        pricePublic: 0,
-        price6LastMonth: [
-            {
-                month: "january",
-                priceMonth: 9,
-            },
-            {
-                month: "February",
-                priceMonth: 11,
-            },
-            {
-                month: "March",
-                priceMonth: 10,
-            },
-            {
-                month: "April",
-                priceMonth: 10,
-            },
-            {
-                month: "May",
-                priceMonth: 13,
-            }, 
-            {
-                month: "June",
-                priceMonth: 12,
-            }
-        ],
-        
-        soldThisWeek: 2,
-        caducityDate: 01/04/2021,
+        name: 'Atun', 
+        description: 'Una lata de atun',
+        price: 10.00,
+        clasification: 'Enlatado',
+        earningPorcentage: '.20',
+        priceSales: 0,
+        sixMonthsPrices: [11.20, 11.50, 11.50, 11.80, 12.00, 12.20],
+        quantitySold: 70,
+        caducity: '2024-06-12'
     },
     {
-        name: "Arroz",
-        description: "Bolsa",
-        price: 15,
-        clasification: "perecedero",
-        earning: 20,
-        pricePublic: 0,
-        price6LastMonth: [
-            {
-                month: "january",
-                priceMonth: 12,
-            },
-            {
-                month: "February",
-                priceMonth: 12,
-            },
-            {
-                month: "March",
-                priceMonth: 13,
-            },
-            {
-                month: "April",
-                priceMonth: 13,
-            },
-            {
-                month: "May",
-                priceMonth: 14,
-            }, 
-            {
-                month: "June",
-                priceMonth: 14,
-            }
-        ],
-        
-        soldThisWeek: 7,
-        caducityDate: 01/04/2021,
+        name: 'Pan de caja', 
+        description: 'Una caja de pan',
+        price: 31.0,
+        clasification: 'Pan',
+        earningPorcentage: '.25',
+        priceSales: 0,
+        sixMonthsPrices: [40.0, 40.20, 40.50, 40.50, 40.80, 41.0],
+        quantitySold: 20,
+        caducity: '2021-05-02'
+    },
+    {
+        name: 'Aceite Nutrioli', 
+        description: 'Una botella de aceite de 1 lt',
+        price: 25.0,
+        clasification: 'Aceite',
+        earningPorcentage: '.15',
+        priceSales: 0,
+        sixMonthsPrices: [28.0, 28.20, 28.20, 29.00, 29.50, 29.90],
+        quantitySold: 30,
+        caducity: 'undefined'
     }
 ]
-
-function Product( name, description, price, clasification, earning, pricePublic, price6LastMonth, soldThisWeek, caducityDate) {
+function Products(name, description, price, clasification, earningPorcentage, sixMonthsPrices=[], quantitySold, caducity, caducityDays) {
     this.name = name;
-    this.description;
-    this.price;
-    
-
+    this.description = description;
+    this.price = price;
+    this.clasification = clasification;
+    this.earningPorcentage = earningPorcentage;
+    this.sixMonthsPrices = sixMonthsPrices;
+    this.quantitySold = quantitySold;
+    this.caducity = caducity;
+    this.caducityDays = caducityDays;
+    this.getPriceSales = function() {
+        let precio = this.price;
+        let porcentaje = this.earningPorcentage;
+        let priceSales = (precio * porcentaje) + precio;
+        return priceSales;
+    }
+    this.priceSales = this.getPriceSales();
+    this.getDaysLeft = function() {
+        let today = new Date();
+        console.log(today)
+        let expDay = new Date(caducity);
+        console.log(expDay)
+        let expDate = expDay.getTime();
+        console.log(expDate)
+        let diferencia = expDate - today.getTime();
+        console.log(diferencia)
+        let horasTranscurridas = diferencia / 1000 / 60 / 60;
+        console.log(horasTranscurridas)
+        return horasTranscurridas/24;
+    }
+    this.caducityDays = this.getDaysLeft();
+    this.getAverageMonths = function(){
+        const average = this.sixMonthsPrices.reduce((accum, item) => {
+        return (accum + item);
+        },0) /this.sixMonthsPrices.length;
+        return average
+    }
+    this.averagePriceMonths = this.getAverageMonths();
 }
-
-
+const products = product.map(item => {
+    return new Products(item.name, item.description, item.price, item.clasification, item.earningPorcentage, item.sixMonthsPrices, item.quantitySold, item.caducity);
+});
+console.log(products)
+const aboveFifty = products.filter(item => {
+    return item.quantitySold > 50;
+})
+console.log(aboveFifty)
+const underTen = products.filter(item => {
+    return item.quantitySold < 10;
+})
+console.log(underTen)
+const fifteenDaysOrLess = products.filter(item => {
+    return item.caducityDays < 15;
+})
+console.log(fifteenDaysOrLess)
